f4b32035de5bc9220d3e8952a07f1858da65dd23
prody-dev <rodriguez.christian.dev@gmail.com>
2025-06-03 15:35:36 -0700

    modelos finales

--- backend/core/models.py
+++ backend/core/models.py
@@ -94,7 +94,7 @@ class OrdenTrabajoServicio(models.Model):
     def __str__(self):
         return f"{self.cantidad} x {self.servicio.nombreServicio} en Orden {self.ordenTrabajo.id}"
 
-#--------------- 1. Datos delk sitio de muestreo -------------
+#--------------- 1. Datos del sitio de muestreo -------------
 class SitioMuestreo(models.Model):
     #El nombre de la empresa, se puede obtener directamente de una consulta asi que puede no ser necesario aqui ⬇︎
     #nombreEmpresa = models.CharField(max_length=255)
@@ -123,11 +123,11 @@ class PuntoMuestreo(models.Model):
     #son los ids que mas adelante iran en la cadena de custodia ⬇︎
     identificacionPunto = models.CharField(max_length=50)
     descripcionProceso = models.TextField(max_length=255, null=True, blank=True)
-    origenMuestra = models.CharField(max_length=50, null=True, blank=True)
+    origenMuestra = models.CharField(max_length=50)
     aguaResidualOtros = models.CharField(max_length=50, null=True, blank=True)
-    horasOpera = models.CharField(max_length=50, null=True, blank=True)
-    horasDescarga = models.CharField(max_length=50, null=True, blank=True)
-    frecuenciaDescarga = models.CharField(max_length=50, null=True, blank=True)
+    horasOpera = models.CharField(max_length=50)
+    horasDescarga = models.CharField(max_length=50)
+    frecuenciaDescarga = models.CharField(max_length=50, default='')
     #esto es para la parte de nombre completo y puesto/cargo, podria hacerse de otra forma ⬇︎
     informacionProporcionada = models.CharField(max_length=50, null=True, blank=True)
     aguaResidualTratamientos = models.ForeignKey(AguaResidualTratamientos, on_delete=models.CASCADE)
@@ -236,6 +236,7 @@ class TemperaturaMuestra(models.Model):
     temp3 = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
     def __str__(self):
         temp_values = [self.temp1, self.temp2, self.temp3]
+        
 class ConductividadMuestra(models.Model):
     cond1 = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
     cond2 = models.DecimalField(max_digits=10, decimal_places=2, null=True, blank=True)
@@ -299,126 +300,142 @@ class HojaCampo(models.Model):
     # es lo mismo que el atributo identificacionPunto del modelo PuntoMuestreo, podria no ser necesario ⬇︎
     idMuestra = models.CharField(max_length=50)
     #siempre va a ser la norma NOM-002-SEMARNAT-1996/NMX-AA-003-1980 asi que podria evitarse ⬇︎
-    normaReferencia = models.CharField(max_length=50)
+    normaReferencia = models.CharField(max_length=50)# debe ser un modelo
     # se refiere a si es Bajo techo o Intemperie/Cielo abierto ⬇︎
-    condicionMuestreo = models.CharField(max_length=50)
+    condicionMuestreo = models.BooleanField(max_length=50)
     fechaMuestreo = models.DateField()
+    MuestraHojaCampo = models.ForeignKey(MuestraHojaCampo, on_delete=models.CASCADE, null=True, blank=True)
+    observaciones = models.TextField(max_length=255, null=True, blank=True)
+    # Podria no ser necesario si se trae de una consulta ⬇︎
+    muestreador = models.CharField(max_length=255, null=True, blank=True)
+    supervisor = models.CharField(max_length=255, null=True, blank=True)                                                                             
+## ------------- 6.- CROQUIS DE UBICACION -------------
 
-##  ------------- CADENA DE CUSTODIA -------------
-# class Preservador(models.Model):
-#     nombre = models.CharField(max_length=50)
+class CroquisUbicacion(models.Model):
+    # Podria no ser necesario si se trae de una consulta ⬇︎
+    nombreEmpresa = models.CharField(max_length=255)
+    domicilio = models.CharField(max_length=255)
+    croquis = models.ImageField(upload_to='croquis/', null=True, blank=True)
+    comentario = models.TextField(max_length=255, null=True, blank=True)
 
-#     def __str__(self):
-#         return self.nombre
+    def __str__(self):
+        return f"Croquis de Ubicación - OT: {self.OrdenTrabajo.codigo} - Muestra: {self.idMuestra}"
 
-# class Matriz(models.Model):
-#     codigo = models.CharField(max_length=20)
-#     nombre = models.CharField(max_length=50)
+#  ------------- CADENA DE CUSTODIA -------------
+class Preservador(models.Model):
+    nombre = models.CharField(max_length=50)
 
-#     def __str__(self):
-#         return self.codigo + ' - ' + self.nombre
+    def __str__(self):
+        return self.nombre
 
-# class Clave(models.Model):
-#     codigo = models.CharField(max_length=20)
-#     nombre = models.CharField(max_length=50)
+class Matriz(models.Model):
+    codigo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=50)
 
-#     def __str__(self):
-#         return self.codigo + ' - ' + self.nombre
+    def __str__(self):
+        return self.codigo + ' - ' + self.nombre
 
-# class Contenedor(models.Model):
-#     codigo = models.CharField(max_length=20)
-#     nombre = models.CharField(max_length=50)
+class Clave(models.Model):
+    codigo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=50)
 
-#     def __str__(self):
-#         return self.codigo + ' - ' + self.nombre
-# class Parametro(models.Model):
-#     nombre = models.CharField(max_length=50)
+    def __str__(self):
+        return self.codigo + ' - ' + self.nombre
 
-#     def __str__(self):
-#         return self.nombre
+class Contenedor(models.Model):
+    codigo = models.CharField(max_length=20)
+    nombre = models.CharField(max_length=50)
 
-# class Prioridad(models.Model):
-#     codigo = models.CharField(max_length=20)
-#     descripcion = models.CharField(max_length=50)
+    def __str__(self):
+        return self.codigo + ' - ' + self.nombre
+class Parametro(models.Model):
+    nombre = models.CharField(max_length=50)
 
-#     def __str__(self):
-#         return self.codigo + ' - ' + self.descripcion\
+    def __str__(self):
+        return self.nombre
+
+class Prioridad(models.Model):
+    codigo = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.codigo + ' - ' + self.descripcion\
             
-# #agregar tabla para idfiltro
-# class Filtro(models.Model):
-#     codigo = models.CharField(max_length=20)
-#     descripcion = models.CharField(max_length=50, null=True, blank=True)
+#agregar tabla para idfiltro
+class Filtro(models.Model):
+    codigo = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=50, null=True, blank=True)
 
-#     def __str__(self):
-#         return self.codigo + ' - ' + self.descripcion
+    def __str__(self):
+        return self.codigo + ' - ' + self.descripcion
 
-# class EstadoCustodia(models.Model):
-#     descripcion = models.CharField(max_length=20)
+class EstadoCustodia(models.Model):
+    descripcion = models.CharField(max_length=20)
 
-#     def __str__(self):
-#         return self.descripcion
+    def __str__(self):
+        return self.descripcion
 
-# class CustodiaExterna(models.Model):
-#     contacto = models.CharField(max_length=50)
-#     puestoCargoContacto = models.CharField(max_length=50, null=True, blank=True)
-#     celularContacto = models.CharField(max_length=50, null=True, blank=True)
-#     correoContacto = models.EmailField(max_length=50, null=True, blank=True)
-#     puntosMuestreoAutorizados = models.CharField(max_length=50)
-#     modificacionOrdenTrabajo = models.BooleanField()
-#     observacionesModificacion = models.TextField(max_length=120, null=True, blank=True)
-#     asesoriaGestionAmbiental = models.BooleanField()
-#     muestreoRequerido = models.CharField(max_length=50)
-#     fechaFinal = models.DateField(null=True, blank=True)
-#     horaFinal = models.TimeField(null=True, blank=True)
-#     muestraCompuesta = models.BooleanField()
-#     idMuestraCompuesta = models.CharField(max_length=50, null=True, blank=True)
-#     muestraPuntual = models.BooleanField()
-#     idMuestraPuntual = models.CharField(max_length=50, null=True, blank=True)
-#     observaciones = models.TextField(max_length=325, null=True, blank=True)
-#     estado = models.ForeignKey(EstadoCustodia, on_delete=models.CASCADE)
-#     prioridad = models.ForeignKey(Prioridad, on_delete=models.CASCADE)
-#     receptor = models.ForeignKey(Receptor, on_delete=models.CASCADE)
-#     ordenTrabajo = models.ForeignKey(OrdenTrabajo, on_delete=models.CASCADE)
+class CustodiaExterna(models.Model):
+    contacto = models.CharField(max_length=50)
+    puestoCargoContacto = models.CharField(max_length=50, null=True, blank=True)
+    celularContacto = models.CharField(max_length=50, null=True, blank=True)
+    correoContacto = models.EmailField(max_length=50, null=True, blank=True)
+    puntosMuestreoAutorizados = models.CharField(max_length=50)
+    modificacionOrdenTrabajo = models.BooleanField()
+    observacionesModificacion = models.TextField(max_length=120, null=True, blank=True)
+    asesoriaGestionAmbiental = models.BooleanField()
+    muestreoRequerido = models.CharField(max_length=50)
+    fechaFinal = models.DateField(null=True, blank=True)
+    horaFinal = models.TimeField(null=True, blank=True)
+    muestraCompuesta = models.BooleanField()
+    idMuestraCompuesta = models.CharField(max_length=50, null=True, blank=True)
+    muestraPuntual = models.BooleanField()
+    idMuestraPuntual = models.CharField(max_length=50, null=True, blank=True)
+    observaciones = models.TextField(max_length=325, null=True, blank=True)
+    estado = models.ForeignKey(EstadoCustodia, on_delete=models.CASCADE)
+    prioridad = models.ForeignKey(Prioridad, on_delete=models.CASCADE)
+    receptor = models.ForeignKey(Receptor, on_delete=models.CASCADE)
+    ordenTrabajo = models.ForeignKey(OrdenTrabajo, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return f"CustodiaExterna - {self.contacto}"
+    def __str__(self):
+        return f"CustodiaExterna - {self.contacto}"
 
-# class Muestra(models.Model):
-#     #nomenclaruta de la identificacion de campo
-#     #nomenclatura-filtro-corrida-contenedor-numero de ducto
-#     identificacionCampo = models.CharField(max_length=50)
-#     fechaMuestreo = models.DateField()
-#     horaMuestreo = models.TimeField()
-#     volumenCantidad = models.CharField(max_length=50, null=True, blank=True)
-#     numeroContenedor = models.CharField(max_length=50)
-#     origenMuestra = models.CharField(max_length=50)
-#     idLaboratorio = models.CharField(max_length=50, null=True, blank=True)
-#     filtro = models.ForeignKey(Filtro, on_delete=models.CASCADE, null=True, blank=True)
-#     preservador = models.ManyToManyField(Preservador, through='PreservadorMuestra')
-#     matriz = models.ForeignKey(Matriz, on_delete=models.CASCADE)
-#     contenedor = models.ForeignKey(Contenedor, on_delete=models.CASCADE)
-#     parametro = models.ForeignKey(Parametro, on_delete=models.CASCADE)
-#     custodiaExterna = models.ForeignKey(CustodiaExterna, on_delete=models.CASCADE)
+class Muestra(models.Model):
+    #nomenclaruta de la identificacion de campo
+    #nomenclatura-filtro-corrida-contenedor-numero de ducto
+    identificacionCampo = models.CharField(max_length=50)
+    fechaMuestreo = models.DateField()
+    horaMuestreo = models.TimeField()
+    volumenCantidad = models.CharField(max_length=50, null=True, blank=True)
+    numeroContenedor = models.CharField(max_length=50)
+    origenMuestra = models.CharField(max_length=50)
+    idLaboratorio = models.CharField(max_length=50, null=True, blank=True)
+    filtro = models.ForeignKey(Filtro, on_delete=models.CASCADE, null=True, blank=True)
+    preservador = models.ManyToManyField(Preservador, through='PreservadorMuestra')
+    matriz = models.ForeignKey(Matriz, on_delete=models.CASCADE)
+    contenedor = models.ForeignKey(Contenedor, on_delete=models.CASCADE)
+    parametro = models.ForeignKey(Parametro, on_delete=models.CASCADE)
+    custodiaExterna = models.ForeignKey(CustodiaExterna, on_delete=models.CASCADE)
 
-#     def __str__(self):
-#         return f"Muestra - {self.identificacionCampo}"
+    def __str__(self):
+        return f"Muestra - {self.identificacionCampo}"
     
-# class PreservadorMuestra(models.Model):
-#     preservador = models.ForeignKey(Preservador, on_delete=models.CASCADE)
-#     muestra = models.ForeignKey(Muestra, on_delete=models.CASCADE)
+class PreservadorMuestra(models.Model):
+    preservador = models.ForeignKey(Preservador, on_delete=models.CASCADE)
+    muestra = models.ForeignKey(Muestra, on_delete=models.CASCADE)
     
-#     def __str__(self):
-#         return f"PreservadorMuestra - {self.preservador} - {self.muestra}"
+    def __str__(self):
+        return f"PreservadorMuestra - {self.preservador} - {self.muestra}"
 
-# #mover a la app del laboratorio
-# class RegistroCustodia(models.Model):
-#     CustodiaExterna = models.ForeignKey(CustodiaExterna, on_delete=models.CASCADE)
-#     entregadoPor = models.CharField(max_length=100)
-#     fechaEntrega = models.DateField()
-#     horaEntrega = models.TimeField()
-#     recibidoPor = models.CharField(max_length=100)
-#     fechaEecepcion = models.DateField()
-#     horaRecepcion = models.TimeField()
+#mover a la app del laboratorio
+class RegistroCustodia(models.Model):
+    CustodiaExterna = models.ForeignKey(CustodiaExterna, on_delete=models.CASCADE)
+    entregadoPor = models.CharField(max_length=100)
+    fechaEntrega = models.DateField()
+    horaEntrega = models.TimeField()
+    recibidoPor = models.CharField(max_length=100)
+    fechaEecepcion = models.DateField()
+    horaRecepcion = models.TimeField()
 
-#     def __str__(self):
-#         return f"Registro de Custodia - Entregado por {self.entregadoPor} a {self.recibidoPor}"
+    def __str__(self):
+        return f"Registro de Custodia - Entregado por {self.entregadoPor} a {self.recibidoPor}"
